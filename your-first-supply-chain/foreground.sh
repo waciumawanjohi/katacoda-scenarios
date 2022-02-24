2bf9fe47190a61a04babe5cbb08c873325b26690
Waciuma Wanjohi <lwanjohi@pivotal.io>
2022-02-24 18:13:42 -0500

    Permissions fix

--- your-first-supply-chain/foreground.sh
+++ your-first-supply-chain/foreground.sh
@@ -9,6 +9,7 @@ install_carto_dependencies() {
   mkdir -p "hack/overlays"
   wget -O- https://raw.githubusercontent.com/vmware-tanzu/cartographer/main/hack/overlays/remove-resource-requests-from-deployments.yaml > hack/overlays/remove-resource-requests-from-deployments.yaml
   wget -O- https://raw.githubusercontent.com/vmware-tanzu/cartographer/waciuma/demo/hack/setup.sh > hack/setup.sh
+  chmod +x hack/setup.sh
   ./hack/setup.sh example-dependencies
 }
 
