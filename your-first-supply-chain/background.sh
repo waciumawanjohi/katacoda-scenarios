b892a071e0521f973eb90704e6fa0a5aac1ce463
Waciuma Wanjohi <lwanjohi@pivotal.io>
2022-02-24 19:05:24 -0500

    background installs

--- your-first-supply-chain/background.sh
+++ your-first-supply-chain/background.sh
@@ -1,10 +1,16 @@
 #/bin/bash
 
+readonly KPACK_VERSION=0.5.1
+readonly SOURCE_CONTROLLER_VERSION=0.17.0
+
 main () {
   install_ytt
   install_kapp
   launch.sh
   install_yq
+  get_overlays
+  install_source_controller
+  install_kpack
 }
 
 install_ytt () {
@@ -21,6 +27,29 @@ install_kapp() {
 
 install_yq() {
   pip3 install yq --ignore-installed
+}
+
+get_overlays() {
+  mkdir -p "overlays"
+  wget -O- https://raw.githubusercontent.com/vmware-tanzu/cartographer/main/hack/overlays/remove-resource-requests-from-deployments.yaml > overlays/remove-resource-requests-from-deployments.yaml
+}
+
+install_source_controller() {
+        kubectl create namespace gitops-toolkit || true
+
+        kubectl create clusterrolebinding gitops-toolkit-admin \
+                --clusterrole=cluster-admin \
+                --serviceaccount=gitops-toolkit:default || true
+
+        ytt --ignore-unknown-comments \
+                -f "overlays/remove-resource-requests-from-deployments.yaml" \
+                -f https://github.com/fluxcd/source-controller/releases/download/v$SOURCE_CONTROLLER_VERSION/source-controller.crds.yaml \
+                -f https://github.com/fluxcd/source-controller/releases/download/v$SOURCE_CONTROLLER_VERSION/source-controller.deployment.yaml |
+                kapp deploy --yes -a gitops-toolkit --into-ns gitops-toolkit -f-
+}
+
+install_kpack() {
+  kubectl apply -f https://github.com/pivotal/kpack/releases/download/v$KPACK_VERSION/release-$KPACK_VERSION.yaml
 }
 
 main
--- your-first-supply-chain/foreground.sh
+++ your-first-supply-chain/foreground.sh
@@ -1,36 +1,7 @@
 #/bin/bash
 
-readonly KPACK_VERSION=0.5.1
-readonly SOURCE_CONTROLLER_VERSION=0.17.0
-
 main () {
   launch.sh
-  get_overlays
-  install_source_controller
-  install_kpack
-}
-
-get_overlays() {
-  mkdir -p "overlays"
-  wget -O- https://raw.githubusercontent.com/vmware-tanzu/cartographer/main/hack/overlays/remove-resource-requests-from-deployments.yaml > overlays/remove-resource-requests-from-deployments.yaml
-}
-
-install_source_controller() {
-        kubectl create namespace gitops-toolkit || true
-
-        kubectl create clusterrolebinding gitops-toolkit-admin \
-                --clusterrole=cluster-admin \
-                --serviceaccount=gitops-toolkit:default || true
-
-        ytt --ignore-unknown-comments \
-                -f "overlays/remove-resource-requests-from-deployments.yaml" \
-                -f https://github.com/fluxcd/source-controller/releases/download/v$SOURCE_CONTROLLER_VERSION/source-controller.crds.yaml \
-                -f https://github.com/fluxcd/source-controller/releases/download/v$SOURCE_CONTROLLER_VERSION/source-controller.deployment.yaml |
-                kapp deploy --yes -a gitops-toolkit --into-ns gitops-toolkit -f-
-}
-
-install_kpack() {
-  kubectl apply -f https://github.com/pivotal/kpack/releases/download/v$KPACK_VERSION/release-$KPACK_VERSION.yaml
 }
 
 main
